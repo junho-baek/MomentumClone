0b72c8de1e9a701c124f2ff8c96dc2cd41369085
junhoBaek <86907815+junho-baek@users.noreply.github.com>
2022-02-20 06:03:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//#3.1에서 배운 내용
+
+//const TITLE = document.getElementById("title");
+
+//console.dir(TITLE);
+
+//TITLE.innerText = "Got you!";
+
+//console.log(TITLE.id);
+//console.log(TITLE.className);
+
+
+//#3.2
+
+//클래스네임으로 불러오기
+const HELLOS = document.getElementsByClassName("hello");
+
+console.log(HELLOS);
+
+//태그네임으로 불러오기
+const TITLE = document.getElementsByTagName("h1");
+
+
+console.log(TITLE);
