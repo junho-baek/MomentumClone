164be251d0b9b504475386e055737598645efc92
junhoBaek <86907815+junho-baek@users.noreply.github.com>
2022-03-04 15:10:37 +0000

    4.2

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 
-function onLoginBtnClick(){
-  const VALUE = loginInput.value;
-  console.log(loginInput.value);
+
+function onLoginSubmit(event){
+
+  event.preventDefault();
+  const USERNAME = loginInput.value;
+  console.log(event);
+  console.log(USERNAME);
 }
 
 
-loginButton.addEventListener("click", onLoginBtnClick );
+loginForm.addEventListener("submit", onLoginSubmit );
